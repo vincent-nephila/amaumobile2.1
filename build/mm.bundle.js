463787c1a732e2a6e63cd94e90e40ad504734c30
vincent-nephila <vincent@nephilaweb.com.ph>
2016-02-22 01:47:22 -0800

    logout redirecting

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -7991,15 +7991,18 @@ angular.module('mm.core.settings')
 }]);
 
 angular.module('mm.core.sidemenu')
-.controller('mmSideMenuCtrl', ["$scope", "$state", "$mmSideMenuDelegate", "$mmSitesManager", "$mmSite", "$mmEvents", "$timeout", "mmCoreEventLanguageChanged", "mmCoreEventSiteUpdated", function($scope, $state, $mmSideMenuDelegate, $mmSitesManager, $mmSite, $mmEvents,
-            $timeout, mmCoreEventLanguageChanged, mmCoreEventSiteUpdated) {
+.controller('mmSideMenuCtrl', ["$scope", "$state", "$mmSideMenuDelegate", "$mmSitesManager", "$mmSite", "$mmEvents", "$timeout", "$mmLoginHelper", "mmCoreEventLanguageChanged", "mmCoreEventSiteUpdated", function($scope, $state, $mmSideMenuDelegate, $mmSitesManager, $mmSite, $mmEvents,
+            $timeout, $mmLoginHelper, mmCoreEventLanguageChanged, mmCoreEventSiteUpdated) {
     $scope.handlers = $mmSideMenuDelegate.getNavHandlers();
     $scope.areNavHandlersLoaded = $mmSideMenuDelegate.areNavHandlersLoaded;
     $scope.siteinfo = $mmSite.getInfo();
     $scope.logout = function() {
+	/*
         $mmSitesManager.logout().finally(function() {
-            $state.go('mm_login.credentials');
+            $state.go('mm_login.sites');
         });
+	*/
+	$mmLoginHelper.goToAddSite();
     };
     $mmSite.getDocsUrl().then(function(docsurl) {
         $scope.docsurl = docsurl;
--- core/components/sidemenu/controllers/menu.js
+++ core/components/sidemenu/controllers/menu.js
@@ -22,18 +22,20 @@ angular.module('mm.core.sidemenu')
  * @name mmSideMenuCtrl
  */
 .controller('mmSideMenuCtrl', function($scope, $state, $mmSideMenuDelegate, $mmSitesManager, $mmSite, $mmEvents,
-            $timeout, mmCoreEventLanguageChanged, mmCoreEventSiteUpdated) {
+            $timeout, $mmLoginHelper, mmCoreEventLanguageChanged, mmCoreEventSiteUpdated) {
 
     $scope.handlers = $mmSideMenuDelegate.getNavHandlers();
     $scope.areNavHandlersLoaded = $mmSideMenuDelegate.areNavHandlersLoaded;
     $scope.siteinfo = $mmSite.getInfo();
 
-    /*$scope.logout = function() {
-	
+    $scope.logout = function() {
+	/*
        $mmSitesManager.logout().finally(function() {
-           $state.go('mm_login.site');
+           $state.go('mm_login.sites');
        });
-    };*/
+	*/
+	$mmLoginHelper.goToAddSite();
+    };
 
     $mmSite.getDocsUrl().then(function(docsurl) {
         $scope.docsurl = docsurl;
